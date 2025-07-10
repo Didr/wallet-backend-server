3bc1aab3dea7573455fe88a26795c06c14e4b083
Didr <Didr@users.noreply.github.com>
2025-07-10 21:22:53 +0200

    Allow using envvars for config
    Still allows using the ecosystem.js config replacer

--- config/config.template.ts
+++ config/config.template.ts
@@ -1,27 +1,26 @@
 export const config = {
-	url: "SERVICE_URL",
-	port: "SERVICE_PORT",
-	appSecret: "SERVICE_SECRET",
-	ssl: "SSL_FLAG",
+	url: process.env.WALLET_BACKEND_URL || "SERVICE_URL",
+	port: process.env.WALLET_BACKEND_PORT || "SERVICE_PORT", 
+	appSecret: process.env.WALLET_BACKEND_SECRET || "SERVICE_SECRET",
 	db: {
-		host: "DB_HOST",
-		port: "DB_PORT",
-		username: "DB_USER",
-		password: "DB_PASSWORD",
-		dbname: "DB_NAME"
+		host: process.env.WALLET_BACKEND_DATABASE_HOST || "DB_HOST",
+		port: process.env.WALLET_BACKEND_DATABASE_PORT || "DB_PORT", 
+		username: process.env.WALLET_BACKEND_DATABASE_USERNAME || "DB_USER",
+		password: process.env.WALLET_BACKEND_DATABASE_PASSWORD || "DB_PASSWORD",
+		dbname: process.env.WALLET_BACKEND_DATABASE_NAME || "DB_NAME"
 	},
-	walletClientUrl: "WALLET_CLIENT_URL",
+	walletClientUrl: process.env.WALLET_BACKEND_CLIENT__URL || "WALLET_CLIENT_URL",
 	webauthn: {
-		attestation: "direct",
-		origin: "WEBAUTHN_ORIGIN",
+		attestation: process.env.WALLET_BACKEND_WEBAUTHN_ATTESTATION || "direct",
+		origin: process.env.WALLET_BACKEND_WEBAUTHN__ORIGIN || "WEBAUTHN_ORIGIN",
 		rp: {
-			id: "WEBAUTHN_RP_ID",
-			name: "wwWallet demo",
+			id: process.env.WALLET_BACKEND_WEBAUTH_NRP__ID || "WEBAUTHN_RP_ID",
+			name: process.env.WALLET_BACKEND_WEBAUTHN_RP__NAME || "wwWallet demo",
 		},
 	},
-	alg: "EdDSA",
+	alg: process.env.WALLET_BACKEND_ALG || "EdDSA",
 	notifications: {
-		enabled: "NOTIFICATIONS_ENABLED",
-		serviceAccount: "firebaseConfig.json"
+		enabled: process.env.WALLET_BACKEND_NOTIFICATIONS__ENABLED || "NOTIFICATIONS_ENABLED",
+		serviceAccount: process.env.WALLET_BACKEND_NOTIFICATIONS__SERVICEACCOUNT || "firebaseConfig.json"
 	}
 }
